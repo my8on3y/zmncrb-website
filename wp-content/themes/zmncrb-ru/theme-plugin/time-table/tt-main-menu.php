2beb645fda3efe0d28ed8f9fd3ee2bc48602dadc
my8on3y <serg.id220489@gmail.com>
2018-10-17 21:59:17 +0700

    update tt-plugin

--- wp-content/themes/zmncrb-ru/theme-plugin/time-table/tt-main-menu.php
+++ wp-content/themes/zmncrb-ru/theme-plugin/time-table/tt-main-menu.php
@@ -1,7 +1,6 @@
 <?php
 
 global $wpdb;
-
 if ( $_POST['tt_apply'] ){
     putChangesIntoDb();
 }
@@ -15,7 +14,7 @@ function putChangesIntoDb() {
             $tt_arr[$i]['end_hour'] = $_POST['end_hour'.$i];
             $tt_arr[$i]['end_minute'] = $_POST['end_minute'.$i];
     }
-    global $wpdb;
+  
     $wpdb -> update( 
         'wp_tt_doctors',
         array ( 'time_table' => json_encode($tt_arr) ),
@@ -38,7 +37,7 @@ foreach ($specArr as $spec_value){
     $queryArr = $wpdb -> get_results("SELECT * FROM wp_tt_doctors WHERE specialty = '$spec_value'");
     echo '<h2>'. $spec_value .  '</h2><br>';
     foreach ($queryArr as $query) {
-        echo '<div class="doc_name">' . $query -> last_name .' '. $query -> name . '</div>';
+        echo '<div class="doc_name">' . $query -> last_name .' '. $query -> name . ' ' .  $query -> patronymic . '</div>';
         echo '<form method="POST">';
         echo '<input type="hidden" name="id" value="' . $query -> id . '">';
         echo '----<input type="checkbox" name="disable">нет приёма<br>';
@@ -53,15 +52,16 @@ foreach ($specArr as $spec_value){
             $query_result = json_decode($query -> time_table, true);
             echo '<div class="time_table_day">';
             echo '<input type="number" name="start_hour'.$i.'" pattern="[0-9]{,2}" placeholder="чч" max="23" min="0" value="' . $query_result[$i]['start_hour'] .'">';
-            echo '<input type="number" name="start_minute'.$i.'" pattern="[0-9]{,2}" placeholder="мм" max="59" min="0" step="10" value="' . $query_result[$i]['start_minute'] .'">' . ':' ;
+            echo '<input type="number" id="minute_field" name="start_minute'.$i.'" pattern="[0-9]{,2}" placeholder="мм" max="59" min="0" step="10" value="' . $query_result[$i]['start_minute'] .'">' . ':' ;
             echo '<input type="number" name="end_hour'.$i.'" pattern="[0-9]{,2}" placeholder="чч" max="23" min="0" value="' . $query_result[$i]['end_hour'] .'">';
-            echo '<input type="number" name="end_minute'.$i.'" pattern="[0-9]{,2}" placeholder="мм" max="59" min="0" step="10" value="' . $query_result[$i]['end_minute'] .'">'; 
+            echo '<input type="number" id="minute_field" name="end_minute'.$i.'" pattern="[0-9]{,2}" placeholder="мм" max="59" min="0" step="10" value="' . $query_result[$i]['end_minute'] .'">'; 
             echo '</div>'; 
         }
         echo '<input type="submit" name="tt_apply" value="ок">';
         echo '</form>';
         echo '<br>';       
     }
+    echo '<hr>';
 }
 
 
@@ -72,6 +72,10 @@ foreach ($specArr as $spec_value){
         width: 44px;
         text-align: center;
         font-size: 12px;
+    }
+
+    #minute_field {
+        color: #808080;
     }
 
     .doc_name {
