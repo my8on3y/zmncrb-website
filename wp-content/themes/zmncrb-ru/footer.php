f581eb923ed8c5b1633ba33c628afdf102560529
my8on3y <serg.id220489@gmail.com>
2018-10-29 22:02:43 +0700

    yandex metrika add

--- wp-content/themes/zmncrb-ru/footer.php
+++ wp-content/themes/zmncrb-ru/footer.php
@@ -55,34 +55,40 @@
 
 				</div>
 				<div class="footer-informer">
-					<!-- Yandex.Metrika counter -->
-						<script type="text/javascript" >
-							(function (d, w, c) {
-								(w[c] = w[c] || []).push(function() {
-									try {
-										w.yaCounter50910626 = new Ya.Metrika2({
-											id:50910626,
-											clickmap:true,
-											trackLinks:true,
-											accurateTrackBounce:true
-										});
-									} catch(e) { }
-								});
+				<!-- Yandex.Metrika informer -->
+<a href="https://metrika.yandex.ru/stat/?id=50910626&amp;from=informer"
+target="_blank" rel="nofollow"><img src="https://informer.yandex.ru/informer/50910626/3_1_FFFFFFFF_FFFFFFFF_0_pageviews"
+style="width:88px; height:31px; border:0;" alt="Яндекс.Метрика" title="Яндекс.Метрика: данные за сегодня (просмотры, визиты и уникальные посетители)" /></a>
+<!-- /Yandex.Metrika informer -->
 
-								var n = d.getElementsByTagName("script")[0],
-									s = d.createElement("script"),
-									f = function () { n.parentNode.insertBefore(s, n); };
-								s.type = "text/javascript";
-								s.async = true;
-								s.src = "https://mc.yandex.ru/metrika/tag.js";
+<!-- Yandex.Metrika counter -->
+<script type="text/javascript" >
+    (function (d, w, c) {
+        (w[c] = w[c] || []).push(function() {
+            try {
+                w.yaCounter50910626 = new Ya.Metrika2({
+                    id:50910626,
+                    clickmap:true,
+                    trackLinks:true,
+                    accurateTrackBounce:true
+                });
+            } catch(e) { }
+        });
 
-								if (w.opera == "[object Opera]") {
-									d.addEventListener("DOMContentLoaded", f, false);
-								} else { f(); }
-							})(document, window, "yandex_metrika_callbacks2");
-						</script>
-						<noscript><div><img src="https://mc.yandex.ru/watch/50910626" style="position:absolute; left:-9999px; alt="Яндекс" /></div></noscript>
-						<!-- /Yandex.Metrika counter -->
+        var n = d.getElementsByTagName("script")[0],
+            s = d.createElement("script"),
+            f = function () { n.parentNode.insertBefore(s, n); };
+        s.type = "text/javascript";
+        s.async = true;
+        s.src = "https://mc.yandex.ru/metrika/tag.js";
+
+        if (w.opera == "[object Opera]") {
+            d.addEventListener("DOMContentLoaded", f, false);
+        } else { f(); }
+    })(document, window, "yandex_metrika_callbacks2");
+</script>
+<noscript><div><img src="https://mc.yandex.ru/watch/50910626" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+<!-- /Yandex.Metrika counter -->
 				</div>
 			</div>
 
